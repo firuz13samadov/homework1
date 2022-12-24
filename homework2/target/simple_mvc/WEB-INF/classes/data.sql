b2dfd15aca87694b10f29ee36a778e75aa23baf3
samadov <samadov_03@inbox.ru>
2022-12-24 16:07:32 +0500

    commit

---
+++ homework2/target/simple_mvc/WEB-INF/classes/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO books(author, title, size) VALUES ('Tom Mount', 'Java', 542)
+INSERT INTO books(author, title, size) VALUES ('Alli Becker', 'Me', 214)
+INSERT INTO books(author, title, size) VALUES ('John Terry', 'Life', 403)
---
+++ homework4/src/test/java/com/example/MyBookShopApp/MyBookShopAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.MyBookShopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
